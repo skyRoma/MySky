9f317001055c9d35c9985fbd11f3f7ea096c46c2
skyRoma <roma.sluka.97@mail.ru>
2019-02-03 03:23:12 +0300

    update scripts

--- git-hook-scripts/check-tests.bash
+++ git-hook-scripts/check-tests.bash
@@ -6,7 +6,7 @@
 # magic line to ensure that we're always inside the root of our application,
 # no matter from which directory we'll run script
 # thanks to it we can just enter `./scripts/run-tests.bash`
-#cd "${0%/*}/.."
+cd "${0%/*}/.."
 
 # let's fake failing test for now 
 echo "Running tests"
