953bd3dd14d2b38b9699ce592337a7e996ec5b2a
Roman Sluka <r.sluka@itransition.com>
2019-03-29 16:14:57 +0300

    MYS-8: add user model and subjects for auth service.

--- client/src/app/core/auth/auth.service.ts
+++ client/src/app/core/auth/auth.service.ts
@@ -1,11 +1,23 @@
 import { Injectable } from '@angular/core';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { HttpClient } from '@angular/common/http';
+import { Observable, BehaviorSubject } from 'rxjs';
+
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(public jwtHelper: JwtHelperService) {}
+  private currentUserSubject: BehaviorSubject<User>;
+  public currentUser: Observable<User>;
+
+  constructor(public jwtHelper: JwtHelperService, private http: HttpClient) {
+    this.currentUserSubject = new BehaviorSubject<User>(
+      JSON.parse(this.getToken())
+    );
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
 
   redirectUrl: string;
 
