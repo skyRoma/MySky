f2f77811fc99176cc09b971c57994e641d3aa82a
Roman <roma.sluka.97@mail.ru>
2019-03-31 02:53:15 +0300

    MYS-8: add interfaces

---
+++ client/src/app/core/interfaces/http-responses.ts
@@ -0,0 +1,4 @@
+export interface MysHttpResponse {
+  success: boolean;
+  msg: string;
+}
---
+++ client/src/app/core/interfaces/token.ts
@@ -0,0 +1,6 @@
+export interface MysToken {
+  sub: string;
+  iat: number;
+  exp: number;
+  role: string;
+}
